88ed0beb2595479aec5aaed714a1c81e03e7dff6
Mohsin Ejaz <mejaz.bese21@gmail.com>
2023-08-16 23:01:57 +0500

    Update index.tsx

--- src/components/Pizzas/ProductShop/ProductItem/index.tsx
+++ src/components/Pizzas/ProductShop/ProductItem/index.tsx
@@ -2,11 +2,11 @@ import PizzaDisplay from '@src/components/pizzas/PizzaDisplay';
 import PizzaForm from '@src/components/pizzas/PizzaForm';
 import styles from './index.module.scss';
 
-const ProductItem = ({visualise}) => {
+const ProductItem = ({pizza}) => {
   return (
     <div className={styles['product-item']}>
-      <PizzaForm>
-        <PizzaDisplay pizza={visualise} />
+      <PizzaForm pizza={pizza}>
+        <PizzaDisplay toppings={pizza.toppings} />
       </PizzaForm>
     </div>
   );
