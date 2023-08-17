2f89ac4e51766cb789169221ba9f359e1fc5cfbb
Mohsin Ejaz <mejaz.bese21@gmail.com>
2023-08-17 10:34:20 +0500

    put assets into public folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import App from '@src/App';
 import ReactDOM from 'react-dom/client';
 
-import '@/assets/css/style.css';
 import '@/src/main.scss';
 
 import { enableReactUse } from '@legendapp/state/config/enableReactUse';
